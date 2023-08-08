b9740968ae7fc52d7f98afcc84797d42fe50f50f
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-08-08 21:17:56 +0530

    added cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Urbanist } from 'next/font/google'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
@@ -29,4 +31,3 @@ export default function RootLayout({
     </html>
   )
 }
-// TODO: continue from add to cart functionality 9:20:30
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,12 +9,14 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+    const cart = useCart();
     const previewModal = usePreviewModal();
     const router = useRouter();
 
@@ -26,6 +28,12 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         event.stopPropagation();
 
         previewModal.onOpen(data);
+    }
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        cart.addItem(data);
     }
 
     return (
@@ -41,7 +49,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
                 <div className="opacity-0 group-hover:opacity-75 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600" />} />
-                        <IconButton onClick={() => {}} icon={<ShoppingCart size={20} className="text-gray-600" />} />
+                        <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-600" />} />
                     </div>
                 </div>
             </div>
